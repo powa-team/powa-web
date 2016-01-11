2456afb5d4682be52524cbf15e33b6568942d246
Julien Rouhaud <julien.rouhaud@free.fr>
2016-01-11 15:40:46 +0100

    Notify that server is starting.
    Thanks to Pierre Hilbert for the feature request.

--- run_powa.py
+++ run_powa.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 from powa import make_app
 import tornado.ioloop
+import logging
 
 if __name__ == "__main__":
     application = make_app(debug=True, gzip=True, compress_response=True)
     application.listen(8888)
+    logger = logging.getLogger("tornado.application")
+    logger.info("Starting powa-web on 127.0.0.1:8888")
     tornado.ioloop.IOLoop.instance().start()
