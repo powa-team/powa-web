0c2282aeab87922b111a773a68f1b77e861c4103
Julien Rouhaud <julien.rouhaud@free.fr>
2016-10-06 12:36:30 +0200

    3.0.2: Version bump and changelog

--- powa/__init__.py
+++ powa/__init__.py
@@ -4,7 +4,7 @@ Powa main application.
 """
 import os
 
-__VERSION__ = '3.0.2'
+__VERSION__ = '3.1.0'
 __VERSION_NUM__ = [int(part) for part in __VERSION__.split('.')]
 POWA_ROOT = os.path.dirname(__file__)
 
