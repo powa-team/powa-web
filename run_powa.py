6cef7016a1ee2826b88adadef8dfaedc8328fd86
Julien Rouhaud <julien.rouhaud@free.fr>
2017-05-04 21:05:12 +0200

    Specify http connection when loggin powa-web start

--- run_powa.py
+++ run_powa.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
     application = make_app(debug=True, gzip=True, compress_response=True)
     application.listen(8888)
     logger = logging.getLogger("tornado.application")
-    logger.info("Starting powa-web on 127.0.0.1:8888")
+    logger.info("Starting powa-web on http://127.0.0.1:8888")
     tornado.ioloop.IOLoop.instance().start()
