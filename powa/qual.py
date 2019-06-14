2c7035637c759d2492dd534fa2baca3179912aff
Ronan Dunklau <ronan@dunklau.fr>
2019-06-14 15:09:54 +0200

    Move the "other queries" panel to its own grid

--- powa/qual.py
+++ powa/qual.py
@@ -4,7 +4,7 @@ Dashboard for the qual page
 from sqlalchemy.sql import text, bindparam
 from tornado.web import HTTPError
 from powa.dashboards import (
-    Dashboard, Graph,
+    Dashboard, Graph, Grid,
     MetricGroupDef, MetricDef,
     DashboardPage, ContentWidget)
 from powa.sql import qual_constants, resolve_quals
@@ -99,13 +99,6 @@ class QualDetail(ContentWidget):
         my_qual = None
         other_queries = {}
 
-        other_queries = {r['queryid']: r['query'] for r in self.execute(text("""
-            SELECT DISTINCT queryid, query
-            FROM powa_qualstats_quals
-            JOIN powa_statements USING (queryid)
-            WHERE qualid = :qual
-            LIMIT 5"""),
-            params={"qual": qual})}
         for qual in quals:
             if qual['is_my_query']:
                 my_qual = resolve_quals(remote_conn, [qual])[0]
@@ -117,8 +110,36 @@ class QualDetail(ContentWidget):
         self.render("database/query/qualdetail.html",
                     qual=my_qual,
                     database=database,
-                    server=server,
-                    other_queries=other_queries)
+                    server=server)
+
+class OtherQueriesMetricGroup(MetricGroupDef):
+    """Metric group showing other queries for this qual."""
+    name = "other_queries"
+    xaxis = "queryid"
+    axis_type = "category"
+    data_url = r"/server/(\d+)/metrics/database/([^\/]+)/query/(-?\d+)/qual/(\d+)/other_queries"
+    query_str = MetricDef(label="Query", type="query", url_attr="url")
+
+
+    @property
+    def query(self):
+        return text("""
+            SELECT distinct queryid, query,
+            query as query_str, pd.srvid
+            FROM powa_qualstats_quals pqs
+            JOIN powa_statements USING (queryid, dbid, srvid, userid)
+            JOIN powa_databases pd ON pd.oid = pqs.dbid AND pd.srvid =
+            pqs.srvid
+            WHERE qualid = :qual
+                AND pqs.queryid != :query
+                AND pd.srvid = :server
+                AND pd.datname = :database""")
+
+    def process(self, val, database=None, **kwargs):
+        val = dict(val)
+        val["url"] = self.reverse_url(
+            "QueryOverview", val["srvid"], database, val["queryid"])
+        return val
 
 
 class QualOverview(DashboardPage):
@@ -128,7 +149,7 @@ class QualOverview(DashboardPage):
 
     base_url = r"/server/(\d+)/database/([^\/]+)/query/(-?\d+)/qual/(\d+)"
     params = ["server", "database", "query", "qual"]
-    datasources = [QualDetail, QualConstantsMetricGroup]
+    datasources = [QualDetail, OtherQueriesMetricGroup, QualConstantsMetricGroup]
     parent = QueryOverview
     title = 'Predicate Overview'
 
@@ -141,6 +162,9 @@ class QualOverview(DashboardPage):
         self._dashboard = Dashboard(
             "Qual %(qual)s",
             [[QualDetail],
+             [Grid("Other queries",
+                   metrics=OtherQueriesMetricGroup.all(),
+                   columns=[])],
              [Graph("Most executed values",
                     metrics=[QualConstantsMetricGroup.occurences],
                     x_label_attr="constants",
