7c35b6265c64a698d4df1459dff359bf40c1ddfb
Pierre GIRAUD <pierre.giraud@dalibo.com>
2019-08-14 08:24:14 +0200

    Make sure messages are dismissable

--- powa/static/js/powa/utils/message.js
+++ powa/static/js/powa/utils/message.js
@@ -1,11 +1,10 @@
 define([], function(){
     return {
       add_message: function(level, message) {
-        $("#messages").append('<div class="alert-box ' + level + '">'
+        $("#messages").append('<div data-alert class="alert-box ' + level + '">'
           + '<ul><li>' + message + '</li></ul>'
           + '<a href="#" class="close">&times;</a>'
           + '</div>');
-        $(document).foundation();
         $(document).foundation('alert', 'reflow');
       },
 
