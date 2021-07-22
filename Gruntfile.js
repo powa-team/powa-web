524b93bd54efbe551466af3742740f6ca0694914
Christoph Dreis <christoph.dreis@innogames.com>
2021-07-22 16:12:26 +0200

    Update build and build dependencies for static JS & CSS files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,91 +1,96 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
+    const sass = require('node-sass');
 
-  // Project configuration.
-  grunt.initConfig({
-    uglify: {
-      options: {
-        banner: '/*! <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'powa/static/build/powa/main.js',
-        dest: 'powa/static/js/powa.min-all.js'
-      }
-    },
-    copy: {
-        cssAsScss: {
-            files: [
-            {
-                expand: true,
-                cwd: 'powa/static/',
-                src: ['bower_components/**/*.css', 'libs/**/*.css', '!**/*.min.css'],
-                dest: 'powa/static/',
-                filter: 'isFile',
-                ext: ".scss"
+    // Project configuration.
+    grunt.initConfig({
+        copy: {
+            cssAsScss: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'powa/static/',
+                        src: ['bower_components/**/*.css', 'libs/**/*.css', '!**/*.min.css'],
+                        dest: 'powa/static/',
+                        filter: 'isFile',
+                        ext: ".scss"
+                    }
+                ]
+            },
+            requirejs: {
+                files: [
+                    {
+                        cwd: 'powa:static/',
+                        src: ['bower_components/requirejs/require.js'],
+                        dest: ['powa/static/js/'],
+                        filter: 'isFile'
+                    }
+                ]
             }
-            ]
         },
         requirejs: {
-            files: [
-            {
-                cwd: 'powa:static/',
-                src: ['bower_components/requirejs/require.js'],
-                dest: ['powa/static/js/'],
-                filter: 'isFile'
+            all: {
+                options: {
+                    baseUrl: "./powa/static/js",
+                    wrap: true,
+                    name: "powa/main",
+                    optimize: "none",
+                    preserveLicenseComments: false,
+                    generateSourceMaps: false,
+                    mainConfigFile: "./powa/static/js/config.js",
+                    out: "powa/static/js/powa.js",
+                }
             }
-            ]
-        }
-    },
-    requirejs: {
-        all: {
+        },
+        sass: {
             options: {
-                baseUrl: "./powa/static/js",
-                wrap: true,
-                name: "powa/main",
-                optimize: "uglify",
-                uglify: {
-                    "except": ["$super"]
-                },
-                preserveLicenseComments: false,
-                generateSourceMaps: false,
-                mainConfigFile: "./powa/static/js/config.js",
-                out: "powa/static/js/powa.min-all.js",
+                implementation: sass
+            },
+            all: {
+                files: {
+                    'powa/static/css/powa-all.min.css': 'powa/static/scss/powa.scss'
+                }
             }
-        }
-    },
-
-    sass: {
-        all: {
-            files: {
-                'powa/static/css/powa-all.min.css': 'powa/static/scss/powa.scss'
+        },
+        uglify: {
+            options: {
+                compress: false,
+                mangle: {
+                    reserved: ['$super']
+                }
+            },
+            all: {
+                files: {
+                    'powa/static/js/powa.min-all.js': ['powa/static/js/powa.js']
+                }
             }
-        }
-    },
+        },
+        bowerRequirejs: {
+            all: {
+                options: {
+                    baseUrl: "",
+                    excludeDev: true,
+                    transitive: true
+                }
+            },
+            target: {
+                rjsConfig: 'powa/static/js/config.js'
+            }
+        },
+        clean: ['powa/static/js/powa.js']
+    });
 
-    bowerRequirejs: {
-      all: {
-        options: {
-          baseUrl: "",
-          excludeDev: true,
-          transitive: true
-        }
-      },
-      target: {
-        rjsConfig: 'powa/static/js/config.js'
-      }
-    }
-  });
 
+    // Load the plugin that provides the "uglify" task.
 
-  // Load the plugin that provides the "uglify" task.
+    // Default task(s).
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-bower-requirejs');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-requirejs');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-sass');
 
-  // Default task(s).
-  grunt.loadNpmTasks('grunt-bower-requirejs');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-requirejs');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-sass');
-
-  grunt.registerTask('default', ['bowerRequirejs', 'copy', 'sass']);
-  grunt.registerTask('dist', ['bowerRequirejs', 'copy', 'requirejs', 'sass']);
+    grunt.registerTask('default', ['bowerRequirejs', 'copy', 'sass']);
+    grunt.registerTask('dist', ['bowerRequirejs', 'copy', 'requirejs', 'uglify', 'sass', 'clean']);
 
 };
