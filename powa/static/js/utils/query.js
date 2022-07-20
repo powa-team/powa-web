903fb94842d6c3cf0afa65dac88b0c211056266f
Pierre GIRAUD <pierre.giraud@dalibo.com>
2022-07-20 11:22:30 +0200

    Complete rewrite of the JS part of the UI
    With this commit the previous frameworks:
     - backbone,
     - foundation,
     - rickshaw,
     - bower.
    
    Are replaced by:
     - VueJS,
     - Vuetify,
     - d3,
     - vitejs.
    
    Co-authored-by: Marion Giusti <marion.giusti@dalibo.com>
    Co-authored-by: Pierre-Louis GONON <pierre-louis.gonon@dalibo.com>

---
+++ powa/static/js/utils/query.js
@@ -0,0 +1,8 @@
+function encodeQueryData(data) {
+  const ret = [];
+  for (let d in data)
+    ret.push(encodeURIComponent(d) + "=" + encodeURIComponent(data[d]));
+  return ret.join("&");
+}
+
+export { encodeQueryData };
