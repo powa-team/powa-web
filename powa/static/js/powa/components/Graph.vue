5f20b7c411774aa8095fdceeb8a8871c75fb8792
Marion Giusti <marion.giusti@dalibo.com>
2022-08-03 17:30:41 +0200

    Change API from Options to Composition for Graph

--- powa/static/js/powa/components/Graph.vue
+++ powa/static/js/powa/components/Graph.vue
@@ -4,7 +4,7 @@
       <h4 class="card-title">
         {{ config.title }}
         <i
-          ref="help"
+          ref="helpEl"
           class="fa fa-info"
         />
       </h4>
@@ -17,10 +17,9 @@
   </div>
 </template>
 
-<script>
-
+<script setup>
+import { onMounted, ref } from 'vue'
 import * as _ from "lodash";
-import MetricWidget from './MetricWidget.vue';
 import * as echarts from "echarts";
 import size from '../utils2/size';
 import store from '../store';
@@ -28,101 +27,197 @@ import moment from 'moment';
 import '../utils2/precisediff';
 import tippy from 'tippy.js';
 import 'tippy.js/dist/tippy.css';
+import $ from "jquery";
 
-export default {
-  extends: MetricWidget,
+const props = defineProps(["config"])
 
-  data: () => {
-    return {
-      axisFormats: {
-        //"number": Rickshaw.Fixtures.Number.formatKMBT,
-        "size": new size.SizeFormatter().fromRaw,
-        "sizerate": function(value){ return new size.SizeFormatter({suffix: "ps"}).fromRaw(value)},
-        "duration": function(data){
-          return moment(parseFloat(data, 10)).preciseDiff(moment(0))
-        },
-        "percent": function(value){ return Math.round(value * 100) / 100 + '%'}
-      }
-    }
+const axisFormats = {
+  //"number": Rickshaw.Fixtures.Number.formatKMBT,
+  "size": new size.SizeFormatter().fromRaw,
+  "sizerate": function(value){ return new size.SizeFormatter({suffix: "ps"}).fromRaw(value)},
+  "duration": function(data){
+    return moment(parseFloat(data, 10)).preciseDiff(moment(0))
   },
+  "percent": function(value){ return Math.round(value * 100) / 100 + '%'}
+}
 
-  methods: {
+const graphContainer = ref(null)
 
-    dataLoaded(data) {
-      // Help
-      this.initGraphHelp();
+const helpEl = ref(null)
 
-      const chart = echarts.init(this.$refs.graphContainer);
 
-      const yAxis = {}
-      const metrics = _.map(this.config.metrics, (metric) => {
-        return metric.split('.')[1];
+onMounted(() => {
+  loadData();
+})
+
+function loadData() {
+  const metricGroup = _.uniq(_.map(props.config.metrics, (metric) => {
+    return metric.split('.')[0];
+  }));
+  const metrics = _.map(props.config.metrics, (metric) => {
+    return metric.split('.')[1];
+  });
+  const toDate = moment();
+  const fromDate = toDate.clone().subtract(1, 'hour');
+  const params = {
+    from: fromDate.format("YYYY-MM-DD HH:mm:ssZZ"),
+    to: toDate.format("YYYY-MM-DD HH:mm:ssZZ")
+  };
+  const sourceConfig = store.dataSources[metricGroup];
+  const grouper = props.config.grouper || null;
+  const xaxis = sourceConfig.xaxis;
+  const yaxis = sourceConfig.yaxis;
+  $.ajax({
+    url: sourceConfig.data_url + '?' + $.param(params)
+  }).done((response) => {
+    const seriesByMetric = {}
+
+    _.each(metrics, (metric) => {
+      seriesByMetric[metric] = {};
+    });
+
+    if (response.messages !== undefined) {
+      $.each(response.messages, function(level, arr) {
+        $.each(arr, function(i) {
+          msg = Message.add_message(level, arr[i]);
+          $("#messages").append(msg);
+        });
       });
-      _.each(metrics, (metric, index) => {
-        const type = this.getType(metric);
-        if (yAxis[type] == undefined) {
-          const formatter = this.axisFormats[type];
-          yAxis[type] = {
-            type: "value",
-            alignTicks: true,
-            axisLabel: {
-              formatter: formatter,
-            }
+    }
+
+    _.each(response.data, (row) => {
+      const group = grouper || "";
+      _.each(metrics, (metric) => {
+        const series = seriesByMetric[metric];
+        let current_group = series[group];
+        if (current_group === undefined) {
+          current_group = series[group] = {
+            metric: metric,
+            id: metric + group,
+            // name: metric.label_template({group: group}),
+            data: []
           }
         }
+        if (row[xaxis] === undefined) {
+          throw "Data is lacking for xaxis. Did you include " + xaxis + " column in your query ?";
+        }
+        current_group.data.push($.extend({}, {x: new Date(row[xaxis] * 1000), y: row[sourceConfig.metrics[metric].yaxis]}, row));
       });
+    });
 
-      const series = _.map(data, (serie) => {
-        return {
-          type: "line",
-          name: serie.label,
-          data: _.map(serie.data, (d) => [d.x, d.y]),
-          yAxisIndex: _.keys(yAxis).indexOf(serie.type)
-        };
-      });
-
-      const option = {
-        tooltip: {
-          trigger: "axis",
-          position: function (pt) {
-            return [pt[0], '10%'];
-          }
-        },
-        legend: {
-          data: _.map(series, (serie) => serie.name),
-        },
-        xAxis: {
-          type: "time",
-        },
-        yAxis: _.map(yAxis, (axis) => axis),
-        series: series
+    let newSeries = [];
+    _.each(metrics, (metric) => {
+      const series = seriesByMetric[metric];
+      if (!$.isEmptyObject(series)) {
+        $.each(series, function(key, serie){
+          const newSerie = $.extend({}, sourceConfig.metrics[metric], serie);
+          newSeries.push(newSerie);
+        });
       }
-      chart.setOption(option);
-    },
+    });
+    dataLoaded(newSeries);
+  }).fail((response) => {
+    console.log ('fail');
+  });
+}
 
-    initGraphHelp() {
-      let labels = '';
-      const metrics = _.map(this.config.metrics, (metric) => {
-        return metric.split('.')[1];
-      });
-      _.each(metrics, (metric, index) => {
-        labels = '<tr>'
-          + '<td><b>' + this.getLabel(metric) + '</b></td>'
-          + '<td>'
-          + this.getDesc(metric)
-          + '</td></td>'
-          + labels;
-      });
-      const help = '<table>' + labels + '</table class="stack">';
-      // add the hover info
-      tippy(this.$refs.help, {
-        content: help,
-        arrow: true,
-        maxWidth: '100%',
-        theme: 'translucent',
-        allowHTML: true
-      });
+function getType(metric) {
+  const metricGroup = _.uniq(_.map(props.config.metrics, (metric) => {
+    return metric.split('.')[0];
+  }));
+  const sourceConfig = store.dataSources[metricGroup];
+  return sourceConfig.metrics[metric].type || 'number';
+}
+
+function getLabel(metric) {
+  const metricGroup = _.uniq(_.map(props.config.metrics, (metric) => {
+    return metric.split('.')[0];
+  }));
+  const sourceConfig = store.dataSources[metricGroup];
+  return sourceConfig.metrics[metric].label;
+}
+
+function getDesc(metric) {
+  const metricGroup = _.uniq(_.map(props.config.metrics, (metric) => {
+    return metric.split('.')[0];
+  }));
+  const sourceConfig = store.dataSources[metricGroup];
+  return sourceConfig.metrics[metric].desc;
+}
+
+function dataLoaded(data) {
+  // Help
+  initGraphHelp();
+
+  const chart = echarts.init(graphContainer.value);
+
+  const yAxis = {}
+  const metrics = _.map(props.config.metrics, (metric) => {
+    return metric.split('.')[1];
+  });
+  _.each(metrics, (metric, index) => {
+    const type = getType(metric);
+    if (yAxis[type] == undefined) {
+      const formatter = axisFormats[type];
+      yAxis[type] = {
+        type: "value",
+        alignTicks: true,
+        axisLabel: {
+          formatter: formatter,
+        }
+      }
     }
+  });
+
+  const series = _.map(data, (serie) => {
+    return {
+      type: "line",
+      name: serie.label,
+      data: _.map(serie.data, (d) => [d.x, d.y]),
+      yAxisIndex: _.keys(yAxis).indexOf(serie.type)
+    };
+  });
+
+  const option = {
+    tooltip: {
+      trigger: "axis",
+      position: function (pt) {
+        return [pt[0], '10%'];
+      }
+    },
+    legend: {
+      data: _.map(series, (serie) => serie.name),
+    },
+    xAxis: {
+      type: "time",
+    },
+    yAxis: _.map(yAxis, (axis) => axis),
+    series: series
   }
+  chart.setOption(option);
+}
+
+function initGraphHelp() {
+  let labels = '';
+  const metrics = _.map(props.config.metrics, (metric) => {
+    return metric.split('.')[1];
+  });
+  _.each(metrics, (metric, index) => {
+    labels = '<tr>'
+      + '<td><b>' + getLabel(metric) + '</b></td>'
+      + '<td>'
+      + getDesc(metric)
+      + '</td></td>'
+      + labels;
+  });
+  const help = '<table>' + labels + '</table class="stack">';
+  // add the hover info
+  tippy(helpEl.value, {
+    content: help,
+    arrow: true,
+    maxWidth: '100%',
+    theme: 'translucent',
+    allowHTML: true
+  });
 }
 </script>
