0cdc2321d776f2970e78bf75ae990fcd5763bccc
Marion Giusti <marion.giusti@dalibo.com>
2022-08-03 16:59:42 +0200

    Change API from Options to Composition for Grid

--- powa/static/js/powa/components/Grid.vue
+++ powa/static/js/powa/components/Grid.vue
@@ -31,9 +31,9 @@
   </div>
 </template>
 
-<script>
+<script setup>
 
-import Widget from './Widget.vue';
+import { computed, onMounted, ref } from 'vue'
 import store from '../store';
 import * as _ from 'lodash';
 import $ from "jquery";
@@ -44,115 +44,96 @@ import 'highlight.js/styles/default.css';
 import pgsql from 'highlight.js/lib/languages/pgsql';
 hljs.registerLanguage('pgsql', pgsql);
 
-export default {
-  extends: Widget,
-  data: () => {
-    return {
-      items: null,
-      totalRows: 1,
-      currentPage: 1,
-      perPage: 20,
-      filter: null
-    }
-  },
+const props = defineProps(["config"])
 
-  mounted() {
-    this.loadData();
-  },
+const items = ref(null)
 
-  computed: {
-    fields() {
-      const metricGroup = _.uniq(_.map(this.config.metrics, (metric) => {
-        return metric.split('.')[0];
-      }));
-      const metrics = _.map(this.config.metrics, (metric) => {
-        return metric.split('.')[1];
-      });
-      const sourceConfig = store.dataSources[metricGroup];
+onMounted(() => {
+  loadData();
+})
 
-      const columns = this.config.columns;
-      _.each(metrics, function(metric) {
-        columns.push($.extend({}, sourceConfig.metrics[metric]));
-      });
-      _.each(columns, (c) => {
-        $.extend(c, {
-          key: c.name,
-          label: c.label,
-          formatter: this.getFormatter(c.type),
-          class: c.type
-        })
-      });
-      return columns;
-    }
-  },
+const fields = computed(() => {
+  const metricGroup = _.uniq(_.map(props.config.metrics, (metric) => {
+    return metric.split('.')[0];
+  }));
+  const metrics = _.map(props.config.metrics, (metric) => {
+    return metric.split('.')[1];
+  });
+  const sourceConfig = store.dataSources[metricGroup];
 
-  methods: {
-    loadData() {
-      const metricGroup = _.uniq(_.map(this.config.metrics, (metric) => {
-        return metric.split('.')[0];
-      }));
-      const sourceConfig = store.dataSources[metricGroup];
-      const toDate = moment();
-      const fromDate = toDate.clone().subtract(1, 'hour');
-      const params = {
-        from: fromDate.format("YYYY-MM-DD HH:mm:ssZZ"),
-        to: toDate.format("YYYY-MM-DD HH:mm:ssZZ")
-      };
-      $.ajax({
-        url: sourceConfig.data_url + '?' + $.param(params)
-      }).done((response) => {
-        this.dataLoaded(response.data);
-      });
-    },
+  const columns = props.config.columns;
+  _.each(metrics, function(metric) {
+    columns.push($.extend({}, sourceConfig.metrics[metric]));
+  });
+  _.each(columns, (c) => {
+    $.extend(c, {
+      key: c.name,
+      label: c.label,
+      formatter: getFormatter(c.type),
+      class: c.type
+    })
+  });
+  return columns;
+})
 
-    dataLoaded(data) {
-      this.items = data;
-      this.totalRows = this.items.length;
-    },
+function loadData() {
+  const metricGroup = _.uniq(_.map(props.config.metrics, (metric) => {
+    return metric.split('.')[0];
+  }));
+  const sourceConfig = store.dataSources[metricGroup];
+  const toDate = moment();
+  const fromDate = toDate.clone().subtract(1, 'hour');
+  const params = {
+    from: fromDate.format("YYYY-MM-DD HH:mm:ssZZ"),
+    to: toDate.format("YYYY-MM-DD HH:mm:ssZZ")
+  };
+  $.ajax({
+    url: sourceConfig.data_url + '?' + $.param(params)
+  }).done((response) => {
+    dataLoaded(response.data);
+  });
+}
 
-    formatBool(value) {
-      return value ? '✓' : '✗';
-    },
+function dataLoaded(data) {
+  items.value = data;
+}
 
-    formatDuration(value) {
-      return moment(parseFloat(value, 10)).preciseDiff(moment(0), true);
-    },
+function formatBool(value) {
+  return value ? '✓' : '✗';
+}
 
-    formatSize(value) {
-      return new size.SizeFormatter().fromRaw(value);
-    },
+function formatDuration(value) {
+  return moment(parseFloat(value, 10)).preciseDiff(moment(0), true);
+}
 
-    formatQuery(value) {
-      return hljs.highlightAuto(value, ['pgsql']).value;
-    },
+function formatSize(value) {
+  return new size.SizeFormatter().fromRaw(value);
+}
 
-    getFormatter(type) {
-      switch (type) {
-        case 'bool':
-          return this.formatBool;
-        case 'duration':
-          return this.formatDuration;
-        case 'percent':
-          return (value) => value + '%';
-        case 'query':
-          return this.formatQuery;
-        case 'size':
-          return this.formatSize;
-        default:
-          return (value) => value;
-      }
-    },
+function formatQuery(value) {
+  return hljs.highlightAuto(value, ['pgsql']).value;
+}
 
-    onFiltered(filteredItems) {
-      this.totalRows = filteredItems.length;
-      this.currentPage = 1;
-    },
+function getFormatter(type) {
+  switch (type) {
+    case 'bool':
+      return formatBool;
+    case 'duration':
+      return formatDuration;
+    case 'percent':
+      return (value) => value + '%';
+    case 'query':
+      return formatQuery;
+    case 'size':
+      return formatSize;
+    default:
+      return (value) => value;
+  }
+}
 
-    onRowClicked(row) {
-      if (row.url) {
-        window.location.href = row.url;
-      }
-    }
+function onRowClicked(row) {
+  if (row.url) {
+    window.location.href = row.url;
   }
 }
 </script>
