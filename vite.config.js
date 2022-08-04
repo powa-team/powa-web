95b6f7c812cb66f5171325921b12fd8e4d3047fb
Pierre GIRAUD <pierre.giraud@dalibo.com>
2022-08-04 09:31:25 +0200

    Rename main_entry.js -> main.js

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
   build: {
     manifest: true,
     rollupOptions: {
-      input: "/powa/static/js/powa/main_entry.js",
+      input: "/powa/static/js/powa/main.js",
       output: {
         dir: resolve(__dirname, "powa/static/dist")
       }
