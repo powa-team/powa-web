46c9367c16bca1c0c60e05f24c0590839549a546
Pierre GIRAUD <pierre.giraud@dalibo.com>
2022-10-29 17:17:50 +0200

    Remove v-card-text in tabcontainer v-card
    v-tabs and v-tabs-items can be used directly

--- powa/static/js/components/Tabcontainer.vue
+++ powa/static/js/components/Tabcontainer.vue
@@ -1,25 +1,23 @@
 <template>
   <v-card>
-    <v-card-text>
-      <v-tabs v-model="activeTab" align-with-title role="tablist" show-arrows>
-        <v-tab v-for="(tab, index) in props.config.tabs" :key="'tab' + index">
-          {{ tab.title }}
-        </v-tab>
-      </v-tabs>
-      <v-tabs-items v-model="activeTab">
-        <v-tab-item
-          v-for="(tab, index) in props.config.tabs"
-          :key="'tab_content' + index"
-          :transition="false"
-        >
-          <v-card>
-            <v-card-text>
-              <component :is="widgetComponent(tab.type)" :config="tab" />
-            </v-card-text>
-          </v-card>
-        </v-tab-item>
-      </v-tabs-items>
-    </v-card-text>
+    <v-tabs v-model="activeTab" align-with-title role="tablist" show-arrows>
+      <v-tab v-for="(tab, index) in props.config.tabs" :key="'tab' + index">
+        {{ tab.title }}
+      </v-tab>
+    </v-tabs>
+    <v-tabs-items v-model="activeTab">
+      <v-tab-item
+        v-for="(tab, index) in props.config.tabs"
+        :key="'tab_content' + index"
+        :transition="false"
+      >
+        <v-card>
+          <v-card-text>
+            <component :is="widgetComponent(tab.type)" :config="tab" />
+          </v-card-text>
+        </v-card>
+      </v-tab-item>
+    </v-tabs-items>
   </v-card>
 </template>
 
