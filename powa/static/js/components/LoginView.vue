dab2541127c353f63e6203753b232a63ff6812aa
Pierre GIRAUD <pierre.giraud@dalibo.com>
2023-07-30 18:50:37 +0200

    Get rid of vue components in html templates
    Everything now goes in .vue files.
    
    We don't need to manually declare the Vuetify components anymore.
    
    We now expose handler properties as JSON in template.
    
    We can't use VuetifyResolver for treeshaking because of the
    external component V-Snackbars. But we actually have our own resolver
    which does the same except that it excludes VSnackbars.

---
+++ powa/static/js/components/LoginView.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-container fluid>
+    <v-layout align-center justify-center>
+      <v-flex xs12 sm8 md4>
+        <v-card class="elevation-12">
+          <v-toolbar dark color="primary">
+            <v-toolbar-title>Login form</v-toolbar-title>
+          </v-toolbar>
+          <v-form method="post" action="">
+            <v-card-text>
+              <v-text-field name="user" label="Username"></v-text-field>
+              <v-text-field
+                name="password"
+                label="Password"
+                type="password"
+              ></v-text-field>
+              <v-combobox name="server" label="Server" :items="servers">
+              </v-combobox>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="primary" type="submit">Login</v-btn>
+            </v-card-actions>
+          </v-form>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script setup>
+defineProps({
+  servers: {
+    type: Array,
+    default() {
+      return [];
+    },
+  },
+});
+</script>
