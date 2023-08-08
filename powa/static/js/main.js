c091c29bf3ae9f26f5efc8274415479ec8947058
Pierre GIRAUD <pierre.giraud@dalibo.com>
2023-08-08 09:03:43 +0200

    Get dashboard data from JSON request
    Not by using script tags in the HTML body. This will help loading new
    data without loading a new page.

--- powa/static/js/main.js
+++ powa/static/js/main.js
@@ -1,4 +1,3 @@
-import _ from "lodash";
 import Vue from "vue";
 import store from "@/store";
 import vuetify from "@/plugins/vuetify";
@@ -8,36 +7,11 @@ import dynamicComponents from "@/plugins/powa";
 import "@/../styles/main.scss";
 
 document
-  .querySelectorAll('script[type="text/datasources"]')
-  .forEach(function (el) {
-    const dataSources = JSON.parse(el.innerText);
-    _.each(dataSources, function (dataSource) {
-      try {
-        if (dataSource.type == "metric_group") {
-          dataSource.metrics = _.keyBy(dataSource.metrics, "name");
-        }
-      } catch (e) {
-        console.error(
-          "Could not instantiate metric group. Check the metric group definition"
-        );
-      }
-      store.dataSources[dataSource.name] = dataSource;
-    });
-  });
-
-document
   .querySelectorAll('script[type="text/messages"]')
   .forEach(function (el) {
     const messages = JSON.parse(el.innerText);
     store.addAlertMessages(messages);
   });
-
-document
-  .querySelectorAll('script[type="text/datasource_timeline"]')
-  .forEach(function (el) {
-    store.changesUrl = JSON.parse(el.innerText);
-  });
-store.loadData();
 
 dynamicComponents.install();
 
