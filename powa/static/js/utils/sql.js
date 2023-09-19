b145918d402804dec138fc8b1636b2a0cb6d6683
Pierre-Louis GONON <pierre-louis.gonon@dalibo.com>
2023-09-19 10:19:32 +0200

    Add sqlFormatter to beautify SQL queries

--- powa/static/js/utils/sql.js
+++ powa/static/js/utils/sql.js
@@ -1,9 +1,11 @@
 import hljs from "highlight.js/lib/core";
 import pgsql from "highlight.js/lib/languages/pgsql";
 import "highlight.js/styles/default.css";
+import * as sqlFormatter from "sql-formatter";
 
 hljs.registerLanguage("sql", pgsql);
 
 export function formatSql(value) {
+  value = sqlFormatter.format(value, { language: "postgresql" });
   return hljs.highlightAuto(value, ["sql"]).value;
 }
