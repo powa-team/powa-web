bd812ef89f0e10cd9d32e290703b1168f5f2ee44
Pierre GIRAUD <pierre.giraud@dalibo.com>
2023-12-05 16:28:08 +0100

    Use a composable for the store and for alert messages
    In the datepicker, watch has been replaced by watchEffect for simplicity.

---
+++ powa/static/js/composables/useStoreService.js
@@ -0,0 +1,59 @@
+import { useRoute, useRouter } from "vue-router";
+import { ref } from "vue";
+import { dateMath } from "@grafana/data";
+
+const dataSources = ref({});
+const dashboardConfig = ref(null);
+const handlerConfig = ref({
+  homeUrl: "",
+});
+const changesUrl = ref("");
+const changes = ref([]);
+const breadcrumbs = ref([]);
+const defaultFrom = "now-1h";
+const rawFrom = ref(defaultFrom);
+const defaultTo = "now";
+const rawTo = ref(defaultTo);
+
+const from = ref(dateMath.parse(defaultFrom));
+const to = ref(dateMath.parse(defaultTo), true);
+
+function getUrl(url) {
+  const query = {};
+  if (rawFrom.value != defaultFrom || rawTo.value != defaultTo) {
+    query.from = rawFrom.value;
+    query.to = rawTo.value;
+  }
+  return { path: url, query };
+}
+
+export function useStoreService() {
+  const route = useRoute();
+  const router = useRouter();
+
+  function setFromTo(newFrom = defaultFrom, newTo = defaultTo) {
+    rawFrom.value = newFrom;
+    rawTo.value = newTo;
+    from.value = dateMath.parse(newFrom);
+    to.value = dateMath.parse(newTo);
+    router.push({
+      path: route.path,
+      query: { from: newFrom, to: newTo },
+    });
+  }
+
+  return {
+    dataSources,
+    dashboardConfig,
+    handlerConfig,
+    changesUrl,
+    changes,
+    breadcrumbs,
+    rawFrom,
+    rawTo,
+    from,
+    to,
+    setFromTo,
+    getUrl,
+  };
+}
--- powa/static/js/utils/fetch.js
+++ powa/static/js/utils/fetch.js
@@ -1,13 +1,14 @@
 import { onMounted, ref, watch } from "vue";
-import store from "@/store";
+import { useStoreService } from "@/composables/useStoreService";
 
 export function useFetch(name) {
   const loading = ref(false);
   const data = ref(undefined);
+  const { dataSources } = useStoreService();
 
   onMounted(() => {
     watch(
-      () => store.dataSources,
+      () => dataSources.value,
       () => {
         loadData();
       },
@@ -17,19 +18,12 @@ export function useFetch(name) {
 
   function loadData() {
     loading.value = true;
-    const sourceConfig = store.dataSources[name];
+    const sourceConfig = dataSources.value[name];
     sourceConfig.promise.then((response) => {
       data.value = JSON.parse(response);
       loading.value = false;
     });
   }
-
-  watch(
-    () => store.rawFrom + store.rawTo,
-    () => {
-      loadData();
-    }
-  );
 
   return { loading, data };
 }
