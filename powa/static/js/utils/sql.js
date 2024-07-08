d644af8529bf0ab3cc6a35fdb5c014369998b357
Pierre GIRAUD <pierre.giraud@dalibo.com>
2024-07-08 12:20:47 +0200

    Use formatDialect when using sql-formatter
    This reduces the bundle size a lot since we don't need to build all
    possible languages.
    
    We update sql-formatter version along the way.

--- powa/static/js/utils/sql.js
+++ powa/static/js/utils/sql.js
@@ -1,11 +1,11 @@
 import hljs from "highlight.js/lib/core";
 import pgsql from "highlight.js/lib/languages/pgsql";
 import "highlight.js/styles/default.css";
-import * as sqlFormatter from "sql-formatter";
+import { formatDialect, postgresql } from "sql-formatter";
 
 hljs.registerLanguage("sql", pgsql);
 
 export function formatSql(value) {
-  value = sqlFormatter.format(value, { language: "postgresql" });
+  value = formatDialect(value, { dialect: postgresql });
   return hljs.highlightAuto(value, ["sql"]).value;
 }
