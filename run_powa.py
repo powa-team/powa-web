74d4a89ae04524283e8833f25816ca821c6d8fd7
Pierre GIRAUD <pierre.giraud@dalibo.com>
2024-07-24 09:21:32 +0200

    Make debug mode for run_powa.py optional

--- run_powa.py
+++ run_powa.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 from powa import make_app
-import tornado.ioloop
 from tornado.options import options
 import logging
+import os
+import tornado.ioloop
+
+debug = os.environ.get("DEBUG", "True").lower() != "false"
 
 if __name__ == "__main__":
-    application = make_app(debug=True, gzip=True, compress_response=True)
+    application = make_app(debug=debug, gzip=True, compress_response=True)
     application.listen(8888)
     logger = logging.getLogger("tornado.application")
     logger.info("Starting powa-web on http://127.0.0.1:8888%s",
