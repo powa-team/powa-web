5daaea31201561b124aa79f7b95c2d48fbb398f5
Pierre GIRAUD <pierre.giraud@dalibo.com>
2024-11-14 09:31:22 +0100

    Use @sqltools/formatter to format SQL
    It is less prone to parsing errors, popular (~2M weekly downloads)
    and used in VS code.

--- powa/static/js/utils/sql.js
+++ powa/static/js/utils/sql.js
@@ -1,13 +1,13 @@
 import hljs from "highlight.js/lib/core";
 import pgsql from "highlight.js/lib/languages/pgsql";
 import "highlight.js/styles/default.css";
-import { formatDialect, postgresql } from "sql-formatter";
+import sqlFormatter from "@sqltools/formatter";
 
 hljs.registerLanguage("sql", pgsql);
 
 export function formatSql(value) {
   try {
-    value = formatDialect(value, { dialect: postgresql });
+    value = sqlFormatter.format(value, { language: "postgresql" });
   } catch (error) {
     console.error("Could not format SQL:", "\n", value, "\n", error);
   }
